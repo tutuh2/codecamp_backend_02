9bc8f0a880e722be0352847ceb1e4642a8b73a48
Junyoung Song <junyoungsong@Junyoungui-MacBookAir.local>
2022-06-09 12:14:28 +0900

    메인, 미니 프로젝트 분리

--- main-project/backend/src/apis/cart/dto/cart.input.ts
+++
@@ -1,7 +0,0 @@
-import { InputType, OmitType } from '@nestjs/graphql';
-import { Cart } from '../entities/cart.entity';
-
-@InputType()
-export class CartInput extends OmitType(Cart, ['id'], InputType) {
-    //
-}
--- mini-project/backend/models/token.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const tokenSchema = new mongoose.Schema({
-    token: String,
-    phone: String,
-    isAuth: Boolean
-})
-
-export const Token = mongoose.model("Token", tokenSchema)
